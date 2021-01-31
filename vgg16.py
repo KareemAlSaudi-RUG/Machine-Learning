1f761dab49d78a505cd22578d34d430e69c87c2d
Cosmin Roger Harsulescu <cosminroger@gmail.com>
2021-01-31 16:52:26 +0100

    Fixed the code and added gitignore
    The models / weights saved are too high to push on Github. Hence, the folders have been added to gitignore.

--- vgg16.py
+++ vgg16.py
@@ -16,11 +16,11 @@ def vgg16():
     x = Input(shape = (28, 28, 1))
     y = ZeroPadding2D(padding = (2, 2))(x) 
 
-    y = multiConvLayer(y, 64, 2) 
-    y = multiConvLayer(y, 128, 2) 
-    y = multiConvLayer(y, 256, 3) 
-    y = multiConvLayer(y, 512, 3) 
-    y = multiConvLayer(y, 512, 3) 
+    y = multiConvLayer(y, 64, 2) # size 32x32
+    y = multiConvLayer(y, 128, 2) # size 16x16
+    y = multiConvLayer(y, 256, 3) # size 8x8
+    y = multiConvLayer(y, 512, 3) # size 4x4
+    y = multiConvLayer(y, 512, 3) # size 2x2
     y = Flatten()(y)
     y = Dense(units = 4096, activation='relu')(y)
     y = Dense(units = 4096, activation='relu')(y)
@@ -34,7 +34,7 @@ def multiConvLayer(x, value, n):
     for _ in range(n):
         y = Conv2D(value, (3, 3), padding = 'same')(y)
         y = Activation('relu')(y)
-    y = MaxPooling2D(strides = (2, 2))(y)
+    y = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(y)
     return y
 
 def checkDir():
@@ -63,6 +63,29 @@ def load_mnist():
 
     return (x_train, y_train), (x_val, y_val), (x_test, y_test)
 
+def save_model(model):
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("models/model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("models/model.h5")
+
+    print("Saved model to disk")
+
+def load_model():
+    # load json and create model
+    json_file = open('models/model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("models/model.h5")
+
+    print("Loaded model from disk")
+
+    return loaded_model
+
 def main():
     # Create directories
     checkDir()
@@ -72,12 +95,16 @@ def main():
     model.compile(adam, 'categorical_crossentropy', metrics=['accuracy'])
 
     # Load dataset
-    training_data, validation_data, test_data = load_mnist()
+    training_data, val_data, test_data = load_mnist()
 
     tensorboard = TensorBoard(write_grads=True, write_images=True)
     chkpoint = ModelCheckpoint("models/weights.{epoch:02d}-{val_loss:.2f}.hdf5", save_best_only=True)
 
     # Using 20% of the data as validation data. Used only to verify the correctness of the model, not for training.
-    model.fit(training_data[0], training_data[1], epochs=5, callbacks=[tensorboard, chkpoint], validation_split=0.2)
+    model.fit(training_data[0], training_data[1], validation_data=(val_data[0], val_data[1]), epochs=10, callbacks=[tensorboard, chkpoint])
+    save_model(model)
+
+    score = model.evaluate(test_data[0], test_data[1], verbose=0)
+    print(score)
 
 main()
